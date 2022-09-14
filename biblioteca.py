99f004a547861d6f8a997df6f90857d434f1295f
sbonadiez <101202112+sbonadiez@users.noreply.github.com>
2022-09-14 16:26:41 -0500

    Create biblioteca.py

---
+++ biblioteca.py
@@ -0,0 +1,26 @@
+class User:
+    def _init_(self, name: str) -> None:
+        self.name = name
+   
+    def prestar(self, name:str):
+        pass
+   
+    def devolver(name_str):
+        pass
+   
+    def dislibro():
+        pass
+
+
+
+
+class Bibliotecario:
+    def _init_(self, name: str, libross: list) -> None:
+        self.name = name
+        self.libross = libross
+   
+    def subirlibros(self, nomlibro: str):
+        self.libross.append(name_of_book)
+   
+    def modificar(self, nomlibro: str):
+        self.libross.remove(nomlibro)
